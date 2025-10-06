d18ef5efb64a47cf1df032b1590df3bd6681c15d
kawikita <tomonobu.kawakita@samuraiqalabo.tech>
2025-10-06 16:44:45 +0900

    環境セットアップ周りを整理
    - Cargo.tomlにモジュールを追加
    - フォーマッタの設定ファイルを追加
    - ライセンスファイルと環境セットアップのスクリプトを追加
    - Readme.mdを更新
    - toolsディレクトリを.gitignoreの管理対象外に追加

---
+++ setup.sh
@@ -0,0 +1,14 @@
+apt-get update
+apt-get install -y git curl build-essential ca-certificates
+apt-get autoremove -y
+apt-get clean
+
+curl https://sh.rustup.rs -sSF | sh -s -- -y
+
+
+if [ -f "$HOME/.bashrc" ]; then
+    echo 'source $HOME/.cargo/env' >> $HOME/.bashrc
+fi
+if [ -f "$HOME/.zshrc" ]; then
+    echo 'source $HOME/.cargo/env' >> $HOME/.zshrc
+fi
